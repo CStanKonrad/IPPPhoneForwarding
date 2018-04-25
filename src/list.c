694259d583869878bdc8436d0eea1e275f99710f
CStanKonrad <CStanKonrad@users.noreply.github.com>
2018-04-25 22:31:27 +0200

    dodanie listy

---
+++ src/list.c
@@ -0,0 +1,233 @@
+/** @file
+ * Implementacja listy dwukierunkowej
+ *
+ * @author Konrad Staniszewski
+ * @copyright Konrad Staniszewski
+ * @date 25.04.2018
+ */
+
+#include <stdlib.h>
+
+#include "list.h"
+
+
+/**
+ * @brief Inicjalizuje węzeł @p node.
+ * @param[in] node  - wskaźnik na węzeł.
+ */
+static void listInitNode(ListNode node) {
+    node->previous = NULL;
+    node->next = NULL;
+}
+
+/**
+ * @brief Tworzy i inicjalizuje węzeł listy.
+ * @return Wskaźnik na nowo stworzony i zainicjowany węzeł,
+ *         w przypadku problemów z przydzieleniem pamięci NULL.
+ */
+static ListNode listAllocNode() {
+    size_t bytesToAlloc = sizeof(struct ListNode);
+    ListNode newNode = malloc(bytesToAlloc);
+
+    if (newNode == NULL) {
+        return NULL;
+    } else {
+        listInitNode(newNode);
+        return newNode;
+    }
+
+}
+/**
+ * @brief Inicjalizuje strukturę listy @p list.
+ * @param[in] list  - wskaźnik na listę.
+ */
+static void listInitList(List list) {
+    listInitNode(&list->begin);
+    listInitNode(&list->end);
+    list->begin.next = &list->end;
+    list->end.previous = &list->begin;
+}
+
+List listCreate() {
+    List list;
+    size_t bytesToAlloc = sizeof(struct List);
+
+    list = malloc(bytesToAlloc);
+
+    if (list == NULL) {
+        return NULL;
+    } else {
+        listInitList(list);
+
+        return list;
+    }
+}
+
+ListNode listInsertAfter(ListNode node,
+                         LIST_ELEMENT_TYPE element) {
+    ListNode newNode = listAllocNode();
+
+    if (newNode == NULL) {
+        return NULL;
+    } else {
+
+        newNode->element = element;
+
+        newNode->next = node->next;
+        newNode->previous = node;
+
+        newNode->next->previous = newNode;
+        newNode->previous->next = newNode;
+
+        return newNode;
+    }
+}
+
+ListNode listInsertBefore(ListNode node,
+                          LIST_ELEMENT_TYPE element) {
+    return listInsertAfter(node->previous, element);
+}
+
+ListNode listPushFront(List list,
+                       LIST_ELEMENT_TYPE element) {
+    return listInsertAfter(&list->begin, element);
+
+}
+
+ListNode listPushBack(List list,
+                      LIST_ELEMENT_TYPE element) {
+    return listInsertBefore(&list->end, element);
+}
+
+void listDeleteNode(ListNode node) {
+    node->previous->next = node->next;
+    node->next->previous = node->previous;
+    free(node);
+}
+
+void listPopFront(List list) {
+    listDeleteNode(listFirstNode(list));
+}
+
+void listPopBack(List list) {
+    listDeleteNode(listLastNode(list));
+}
+
+void listJoin(List front, List back) {
+    if (!listIsEmpty(back)) {
+        front->end.previous->next = back->begin.next;
+        back->begin.next->previous = front->end.previous;
+
+        front->end = back->end;
+    }
+    free(back);
+}
+
+/** @brief sprawdza czy @p node jest atrapą / strażnikiem.
+ * @param[in] node   - wskaźnik na węzeł.
+ * @return Niezerowa wartość jeżeli jest  atrapą / strażnikiem,
+ *         w przeciwnym razie zero.
+ */
+static int listIsGuard(ListNode node) {
+    return (node->next == NULL || node->previous == NULL);
+}
+
+void listDeleteContent(List list) {
+    while (!listIsEmpty(list)) {
+        listPopFront(list);
+    }
+}
+
+void listDestroy(List list) {
+    listDeleteContent(list);
+    free(list);
+}
+
+int listIsEmpty(List list) {
+    return list->begin.next == &list->end;
+}
+
+ListNode listFirstNode(List list) {
+    if (listIsEmpty(list)) {
+        return NULL;
+    } else {
+        return list->begin.next;
+    }
+}
+
+ListNode listLastNode(List list) {
+    if (listIsEmpty(list)) {
+        return NULL;
+    } else {
+        return list->end.previous;
+    }
+}
+
+ListNode listNextNode(ListNode node) {
+    if (listIsGuard(node->next)) {
+        return NULL;
+    } else {
+        return node->next;
+    }
+}
+
+ListNode listPreviousNode(ListNode node) {
+    if (listIsGuard(node->previous)) {
+        return NULL;
+    } else {
+        return node->previous;
+    }
+}
+
+LIST_ELEMENT_TYPE listNodeGetValue(ListNode node) {
+    return node->element;
+}
+
+/**
+ *
+ * Wczytuje @p k elementów z listy zaczynając od pozycji @p start
+ * i przesuwając się
+ * za pomocą funkcji @p next do tablicy @p output
+ * W przypadku dojścia do końca listy przerywa wczytywanie.
+ * @param[in] next      - funkcja przesuwająca wskaźnik.
+ * @param[in] start     - wskaźnik na węzeł startowy.
+ * @param[in] k         - liczba elementów do wczytania.
+ * @param[out] output    - tablica do umieszczenia wyniku.
+ */
+static void listReadKElements(
+        ListNode (*next)(ListNode),
+        ListNode start, size_t k,
+        LIST_ELEMENT_TYPE *output) {
+    size_t read = 0;
+    ListNode pos = start;
+
+    while (read < k && pos != NULL) {
+        output[read] = listNodeGetValue(pos);
+        read++;
+        pos = next(pos);
+    }
+}
+
+void listLastKElementsToArray(List list, size_t k,
+                              LIST_ELEMENT_TYPE *output) {
+    listReadKElements(listPreviousNode,
+                      listLastNode(list), k, output);
+}
+
+void listFirstKElementsToArray(List list, size_t k,
+                               LIST_ELEMENT_TYPE *output) {
+    listReadKElements(listNextNode,
+                      listFirstNode(list), k, output);
+}
+
+size_t listSize(List list, size_t maxSize) {
+    ListNode ptr = listFirstNode(list);
+    size_t countedSize = 0;
+
+    while (ptr != NULL && countedSize < maxSize) {
+        countedSize++;
+        ptr = listNextNode(ptr);
+    }
+
+    return countedSize;
+}
---
+++ src/list.h
@@ -0,0 +1,289 @@
+/** @file
+ * Interfejs listy dwukierunkowej
+ *
+ * @author Konrad Staniszewski
+ * @copyright Konrad Staniszewski
+ * @date 25.04.2018
+ */
+
+#ifndef TELEFONY_LIST_H
+#define TELEFONY_LIST_H
+
+#include <stddef.h>
+
+/**
+ * @brief Typ elementów przechowywanych w liście.
+ */
+#define LIST_ELEMENT_TYPE int
+
+/**
+ * @brief Wskaźnik na listę.
+ * @see struct List
+ */
+typedef struct List *List;
+
+/**
+ * @brief Wskaźnik na węzeł listy.
+ * @see struct ListNode
+ */
+typedef struct ListNode *ListNode;
+
+/**
+ * @brief Struktura reprezentująca węzeł listy.
+ */
+struct ListNode {
+    /**
+     * @brief Poprzedni węzeł.
+     * Wskaźnik na poprzedni w kolejności węzeł.
+     */
+    ListNode previous;
+
+    /**
+     * @brief Następny węzeł.
+     * Wskaźnik na następny w kolejności węzeł.
+     */
+    ListNode next;
+
+    /**
+     * @brief Przechowywany element.
+     */
+    LIST_ELEMENT_TYPE element;
+};
+
+/**
+ * @brief Struktura reprezentująca listę.
+ */
+struct List {
+    /**
+     * @brief Strażnik (atrapa).
+     * Atrapa stojąca na początku listy.
+     */
+    struct ListNode begin;
+
+    /**
+     * @brief Strażnik (atrapa).
+     * Atrapa stojąca na końcu listy.
+     */
+    struct ListNode end;
+};
+
+/**
+ * @brief Tworzy nową pustą listę.
+ * #### Złożoność
+ * O(1)
+ * @return Wskaźnik do nowo stworzonej listy lub NULL,
+ *         w przypadku problemów z przydzieleniem pamięci.
+ */
+List listCreate();
+
+/**
+ * @brief Dodaje nowy węzeł za podanym.
+ * Dodaje za węzłem @p node nowo stworzony węzeł
+ * z przechowywaną wartością @p element.
+ * #### Złożoność
+ * O(1)
+ * @param[in] node  - wskaźnik na węzeł.
+ * @param[in] element  - element do wstawienia.
+ * @return Wskaźnik do stworzonego węzła,
+ *         zaś w przypadku problemów z przydzieleniem pamięci NULL.
+ */
+ListNode listInsertAfter(ListNode node,
+                         LIST_ELEMENT_TYPE element);
+
+
+/**
+ * @brief Dodaje nowy węzeł przed podanym.
+ * Dodaje przed węzłem @p node nowo stworzony węzeł
+ * z przechowywaną wartością @p element.
+ * #### Złożoność
+ * O(1)
+ * @param[in] node  - wskaźnik na węzeł.
+ * @param[in] element  - element do wstawienia.
+ * @return Wskaźnik do stworzonego węzła,
+ *         zaś w przypadku problemów z przydzieleniem pamięci NULL.
+ */
+ListNode listInsertBefore(ListNode node,
+                          LIST_ELEMENT_TYPE element);
+
+/**
+ * @brief Dodaje element na początek listy.
+ * Dodaje wezeł z wartością @p element na początek @p list.
+ * #### Złożoność
+ * O(1)
+ * @param[in] list  - wskaźnik na listę.
+ * @param[in] element  - element do wstawienia.
+ * @return W przypadku niemożności przydzielenia pamięci
+ *         NULL, w przeciwnym przypadku
+ *         wskaźnik do stworzonego węzła.
+ */
+ListNode listPushFront(List list,
+                       LIST_ELEMENT_TYPE element);
+
+
+/**
+ * @brief Dodaje element na koniec listy.
+ * Dodaje wezeł z wartością @p element na koniec @p list.
+ * #### Złożoność
+ * O(1)
+ * @param[in] list  - wskaźnik na listę.
+ * @param[in] element  - element do wstawienia.
+ * @return W przypadku niemożności przydzielenia pamięci
+ *          NULL, w przeciwnym przypadku
+ *          wskaźnik do stworzonego węzła.
+ */
+ListNode listPushBack(List list,
+                      LIST_ELEMENT_TYPE element);
+
+/**
+ * @brief Usuwa węzeł @p node z listy.
+ * #### Złożoność
+ * O(1)
+ * @param[in] node  - wskaźnik na węzeł.
+ */
+void listDeleteNode(ListNode node);
+
+/**
+ * @brief Usuwa pierwszy węzeł z listy.
+ * Usuwa pierwszy węzeł z @p list.
+ * Zakłada niepustość @p list.
+ * #### Złożoność
+ * O(1)
+ * @param[in] list  - wskaźnik na listę.
+ */
+void listPopFront(List list);
+
+
+/**
+ * @brief Usuwa ostatni węzeł z listy.
+ * Usuwa ostatni węzeł z @p list.
+ * Zakłada neipustość @p list.
+ * #### Złożoność
+ * O(1)
+ * @param[in] list  - wskaźnik na listę.
+ */
+void listPopBack(List list);
+
+
+/**
+ * @brief Łączy listy.
+ * Dołącza listę @p back na koniec listy @p front,
+ * struktura reprezentująca @p back zostaje usunięta.
+ * #### Złożoność
+ * O(1)
+ * @param[in] front  - wskaźnik na listę.
+ * @param[in] back  - wskaźnik na listę.
+ */
+void listJoin(List front, List back);
+
+/**
+ * @brief Usuwa zawartość listy.
+ * Usuwa zawartość listy @p list.
+ * #### Złożoność
+ * O(ilość elementów w @p list)
+ * @param[in] list  - wskaźnik na listę.
+ */
+void listDeleteContent(List list);
+
+/**
+ * @brief Usuwa strukturę.
+ * Usuwa całą zawartość @p list
+ * oraz strukturę reprezentującą @p list.
+ * #### Złożoność
+ * O(ilość elementów w @p list)
+ * @param[in] list  - wskaźnik na listę.
+ */
+void listDestroy(List list);
+
+/**
+ * @brief Sprawdza czy lista @p list jest pusta.
+ * #### Złożoność
+ * O(1)
+ * @param[in] list  - wskaźnik na listę.
+ * @return Niezerowa wartość jeżeli @p list
+ *         jest pusta.
+ */
+int listIsEmpty(List list);
+
+/**
+ * @brief Zwraca wskaźnik do pierwszego węzła.
+ * #### Złożoność
+ * O(1)
+ * @param[in] list  - wskaźnik na listę.
+ * @return Wskaźnik do pierwszego węzła @p list.
+ *         W przypadku braku węzłów NULL.
+ */
+ListNode listFirstNode(List list);
+
+/**
+ * @brief Zwraca wskaźnik do ostatniego węzła.
+ * #### Złożoność
+ * O(1)
+ * @param[in] list  - wskaźnik na listę.
+ * @return Wskaźnik do ostatniego węzła listy @p list.
+ *         W przypadku braku węzłów NULL.
+ */
+ListNode listLastNode(List list);
+
+/**
+ * @brief Następny w kolejności węzeł.
+ * #### Złożoność
+ * O(1)
+ * @param[in] node  - wskaźnik na węzeł.
+ * @return Wskaźnik do następnego w kolejnośći węzła listy @p list.
+ *         W przypadku braku następnego NULL.
+ */
+ListNode listNextNode(ListNode node);
+
+/**
+ * @brief Poprzedni w kolejności węzeł.
+ * #### Złożoność
+ * O(1)
+ * @param[in] node  - wskaźnik na węzeł.
+ * @return Wskaźnik do poprzedniego w kolejnośći węzła listy @p list.
+ *         W przypadku braku poprzedniego NULL.
+ */
+ListNode listPreviousNode(ListNode node);
+
+/**
+ * @brief Zwraca wartość w węźle.
+ * #### Złożoność
+ * O(1)
+ * @param[in] node  - wskaźnik na węzeł.
+ * @return Zwraca wartość przechowywaną przez węzeł @p node.
+ */
+LIST_ELEMENT_TYPE listNodeGetValue(ListNode node);
+
+/**
+ * @brief Kopiuje ostatnie @p k elementów do tablicy.
+ * Do tablicy @p output zapisje min(@p k, liczba_elementów_listy_list)
+ * ostatnich elementów listy @p list.
+ * @param[in] list      - wskaźnik na listę.
+ * @param[in] k         - liczba elementów do pobrania.
+ * @param[out] output    - tablica do umieszczenia wyniku.
+ */
+void listLastKElementsToArray(List list, size_t k,
+                              LIST_ELEMENT_TYPE *output);
+
+/**
+ * @brief Kopiuje pierwsze @p k elementów do tablicy.
+ * Do tablicy @p output zapisje min(@p k, liczba_elementów_listy_list)
+ * pierwszych elementów listy @p list.
+ * @param[in] list      - wskaźnik na listę.
+ * @param[in] k         - liczba elementów do pobrania.
+ * @param[out] output    - tablica do umieszczenia wyniku.
+ */
+void listFirstKElementsToArray(List list, size_t k,
+                               LIST_ELEMENT_TYPE *output);
+
+/**
+ * @brief Sprawdza rozmiar.
+ * Do sprawdzania rozmiaru listy @p list.
+ * @param[in] list      - wskaźnik na listę.
+ * @param[in] maxSize   - organiczenie.
+ * @return W przypadku, gdy liczba elementów przechowywanych w @p list
+ *         jest mniejsza niż @p maxSize to liczba tych elementów,
+ *         w przeciwnym przypadku @p maxSize.
+ */
+size_t listSize(List list, size_t maxSize);
+
+#endif //TELEFONY_LIST_H
