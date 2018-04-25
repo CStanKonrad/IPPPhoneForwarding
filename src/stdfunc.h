ec679ab8447d714a5180ae88052b415ab87f373e
CStanKonrad <CStanKonrad@users.noreply.github.com>
2018-04-25 22:32:02 +0200

    dodanie standardowych makr

---
+++ src/stdfunc.h
@@ -0,0 +1,32 @@
+/** @file
+ * Pomocne makra.
+ * @author Konrad Staniszewski
+ * @copyright Konrad Staniszewski
+ * @date 25.04.2018
+ */
+
+#ifndef TELEFONY_STDFUNC_H
+#define TELEFONY_STDFUNC_H
+
+/**
+ * @brief Zwraca minimum z dwóch wartości.
+ */
+#define MIN(a, b) (((a)<(b))?(a):(b))
+
+/**
+ * @brief Zwraca maksimum z dwóch wartości.
+ */
+#define MAX(a, b) (((a)>(b))?(a):(b))
+
+/**
+ * @brief Makro pomocnicze.
+ * @see STR
+ */
+#define TO_STRING(x) #x
+
+/**
+ * @brief Zamienia napis na ciąg znaków.
+ */
+#define STR(x) TO_STRING(x)
+
+#endif //TELEFONY_STDFUNC_H
