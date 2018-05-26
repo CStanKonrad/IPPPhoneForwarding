da2ce0f78ef95c3c38623c681dbc6814edcc6b84
CStanKonrad <CStanKonrad@users.noreply.github.com>
2018-05-26 21:48:12 +0200

    script fix

--- phone_forward.sh
+++ phone_forward.sh
@@ -106,7 +106,9 @@ cmd="\"$PROGRAM_PATH\" < $TMP_NEW_INPUT > $TMP_NEW_OUTPUT"
 eval "$cmd"
 
 paste $TMP_NEW_OUTPUT $TMP_RAW_OUTPUT > $TMP_CONCAT
+sed -i -e "s/	/.	/g" $TMP_CONCAT
 
-grep -w -e "$NUMBER" $TMP_CONCAT > $TMP_NEW_OUTPUT
-sed -i -e "s/$NUMBER	//g" $TMP_NEW_OUTPUT
+
+grep -w -e "$NUMBER." $TMP_CONCAT > $TMP_NEW_OUTPUT
+sed -i -e "s/$NUMBER.	//g" $TMP_NEW_OUTPUT
 cat $TMP_NEW_OUTPUT
